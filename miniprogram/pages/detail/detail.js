e42ad8af2694eddf1ff5e0b68c32d096bf1e4ca2
julei <182197845@qq.com>
2019-07-07 11:54:29 +0800

    首页联动详情，弹出规格选择

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -18,6 +18,25 @@ Page({
       "../shop/img/2.jpg",
       "../shop/img/3.jpg"
     ],
+    // 对象
+    detail: {},  
+    showModalStatus: false,//是否显示
+    gg_id: 0,//规格ID
+    gg_txt: '',//规格文本
+    gg_price: 0,//规格价格
+    guigeList: [{ guige: '100', price: '150' }, { guige: '200', price: '150' }, { guige: '300', price: '150' }],
+    num: 1,//初始数量
+  },
+  onLoad: function (options) {
+    var that = this
+    // 字符串转json
+    var info = JSON.parse(options.Mesgs);
+    console.log(info)
+    that.setData({
+      // 把从index页面获取到的属性值赋给详情页的detail，供详情页使用
+      detail: info
+    })
+
   },
   //预览图片
   previewImage: function (e) {
@@ -48,6 +67,89 @@ Page({
       duration: 2000
     });
   },
+
+  filter: function (e) {
+    //console.log(e);
+    var self = this, id = e.currentTarget.dataset.id, txt = e.currentTarget.dataset.txt, price = e.currentTarget.dataset.price
+    self.setData({
+      gg_id: id,
+      gg_txt: txt,
+      gg_price: price
+    });
+  },
+
+  /* 点击减号 */
+  bindMinus: function () {
+    var num = this.data.num;
+    // 如果大于1时，才可以减  
+    if (num > 1) {
+      num--;
+    }
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num <= 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 点击加号 */
+  bindPlus: function () {
+    var num = this.data.num;
+    // 不作过多考虑自增1  
+    num++;
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num < 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+
+  //显示对话框
+  showModal: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  //隐藏对话框
+  hideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+
   addCar: function (e) {
 
     var goods = this.data.goodsList;
--- miniprogram/pages/shop/shop.js
+++ miniprogram/pages/shop/shop.js
@@ -16,65 +16,50 @@ Page({
     duration: 1000,
     goodsList: [
       {
-        id: 132,
+        id: 1,
         imgUrl: 'img/1.jpg',
         title: '黄道益1瓶',
         oldPrice: 75,
-        newPrice: 62
+        newPrice: 62,
+        stock: '99',
+        sales: '12'
       },
       {
-        id: 122,
+        id: 2,
         imgUrl: 'img/2.jpg',
         title: '奶粉',
         oldPrice: 300,
-        newPrice: 260
+        newPrice: 260,
+        stock: '115',
+        sales: '35'
       },
       {
-        id: 302,
+        id: 3,
         imgUrl: 'img/3.jpg',
         title: '雅诗兰黛小棕瓶',
         oldPrice: 1250,
-        newPrice: 928
-      },
-      {
-        id: 132,
-        imgUrl: 'img/1.jpg',
-        title: '黄道益1瓶',
-        oldPrice: 75,
-        newPrice: 62
-      },
-      {
-        id: 122,
-        imgUrl: 'img/2.jpg',
-        title: '奶粉',
-        oldPrice: 300,
-        newPrice: 260
-      },
-      {
-        id: 302,
-        imgUrl: 'img/3.jpg',
-        title: '雅诗兰黛小棕瓶',
-        oldPrice: 1250,
-        newPrice: 928
+        newPrice: 928,
+        stock: '150',
+        sales: '26'
       }
     ]
   },
   onLoad: function () {
-    var that = this
+    //var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
+    //app.getUserInfo(function (userInfo) {
       //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-    })
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          height: res.windowHeight + 'px'
-        });
-      }
-    })
+     // that.setData({
+     //   userInfo: userInfo
+     // })
+    //})
+    //wx.getSystemInfo({
+     // success: function (res) {
+      //  that.setData({
+      //    height: res.windowHeight + 'px'
+       // });
+     // }
+   // })
   },
   search: function (e) {
     wx.navigateTo({
@@ -82,8 +67,32 @@ Page({
     })
   },
   showCar: function (e){
+
+    var that = this
+    //获取当前点击元素的id(索引值)
+    var Id = e.currentTarget.id;
+    //获取当前点击元素的属性值。
+    var mesg = that.data.list[Id];
+    //因为获取到的值是个对象，url只能传字符串，所以必须把它转化为字符串。
+    mesg = JSON.stringify(mesg);
     wx.navigateTo({
-      url: '../detail/detail',
+      url: '../detail/detail?Mesgs=' + mesg,
+    })
+  },
+  // 方法
+  detail: function (e) {
+    var that = this
+    //获取当前点击元素的id(索引值)
+    var Id = e.currentTarget.id;
+    //获取当前点击元素的属性值。
+    var mesg = that.data.goodsList[Id];
+    //因为获取到的值是个对象，url只能传字符串，所以必须把它转化为字符串。
+    mesg = JSON.stringify(mesg);
+    //跳转到详情页 
+    wx.navigateTo({
+      //在接收页面的url后面加上“？自定义名称=字符串”就可以通过url传值
+      url: '../detail/detail?Mesgs=' + mesg,
+
     })
   }
 })
